113168c9fef48d48a445a35a8c5d68ccaea7a400
Alan Synnestvedt <asynnestvedt@gmail.com>
2017-07-17 19:00:54 -0400

    stubbing jwt

--- routes/files.js
+++ routes/files.js
@@ -108,6 +108,20 @@ module.exports = function(app, db) {
             });
         }
     });
+
+
+    /**
+     * JWT test
+     */
+    var jwt = require('express-jwt');
+    app.get('/file/secure',
+        jwt({secret: config.app.secret }),
+        function(req, res) {
+            if (!req.user.admin) return res.sendStatus(401);
+            res.sendStatus(200);
+        });
+
+
     app.get('/file', (req, res) => {
 
         File.find({}, (err, result) => {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 module.exports = function(app, db) {
+
+    //lets get user identity... try JWT
+    // ... no JWT??... check if there is a custom user header "X-Users" with array from ids
+    // .. is it a POST?... maybe there's a UserID ??
+
     require('./files.js')(app, db);
     require('./res.js')(app, db);
 };
